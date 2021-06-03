bcfdac684ba8b3ee718f1b2e6682a2376ec8e29b
Maarten van der Peet <Maarten.van.der.Peet@di.huc.knaw.nl>
2021-06-03 15:27:33 +0200

    flask as server

--- flaskpython/app.py
+++ flaskpython/app.py
@@ -1,5 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
+import json
+
 app = Flask(__name__)
+CORS(app)
+
 @app.route('/')
 def home():
-    return 'Flask with docktje!'
+    return 'Flask with dockertje!'
+
+@app.route('/receive/') # start slash and end slash essential
+def receive():
+    return 'message received'
+
+@app.route('/upload/', methods = ['POST', 'GET']) # POST is not in the default. added GET for tests, OPTIONS is always possible
+# @app.route('/upload/')
+def upload():
+    status = "OK"
+    bashcmd = "fmpeg converted"
+    returnvalue = 0
+    returnvalues = {"storestatus" : status, "mp4conv" : {"bashcmd" : bashcmd, "return" : returnvalue}}
+    # dump() 
+    return json.dumps(returnvalues)
+
+
+# is this necessary with Docker?
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
